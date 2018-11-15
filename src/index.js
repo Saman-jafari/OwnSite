4795fc67972ba32a4568fc7d0311810fc98848ec
Saman-jafari <isaimon@me.com>
2018-11-15 11:51:33 +0100

    index

--- src/index.js
+++ src/index.js
@@ -5,6 +5,5 @@ import registerServiceWorker from "./registerServiceWorker";
 import store from "Redux/store";
 import Root from "./Routes/Routes";
 
-
 ReactDOM.render(<Root store={store} />, document.getElementById("root"));
 registerServiceWorker();
